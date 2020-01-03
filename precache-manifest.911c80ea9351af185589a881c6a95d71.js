f78e46d3def74b12e02f17e6e3680966630223a5
qianlongxu <qianlongxu@sohu-inc.com>
2020-01-03 18:42:48 +0800

    updated:2020-01-03 18:42:48

---
+++ precache-manifest.911c80ea9351af185589a881c6a95d71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901eecf4ab3eb916c1a462f6fda3d74b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1117b90a680abb66a8c",
+    "url": "/static/css/main.3276b470.chunk.css"
+  },
+  {
+    "revision": "2e5f6b75473837d5b67c",
+    "url": "/static/js/2.7cde186d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.7cde186d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1117b90a680abb66a8c",
+    "url": "/static/js/main.52913498.chunk.js"
+  },
+  {
+    "revision": "b2fe8922c8fbd0c10cf4",
+    "url": "/static/js/runtime-main.ec0139ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
